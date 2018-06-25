79c6c05687fc124f6a836ddf1fc52b51e83ec1a5
jenix21 <devfrog@gmail.com>
2018-06-26 00:58:24 +0900

    Initial

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use std::collections::HashMap;
+use std::process;
+
+struct BuildCommand {
+  cmd_map: HashMap<String, &'static str>,
+}
+
+impl BuildCommand {
+  fn new() -> Self {
+    BuildCommand {
+      cmd_map: HashMap::new(),
+    }
+  }
+  fn init(&mut self) {
+    self.cmd_map.insert(String::from("gclient"), "sync --verbose --reset --force --with_branch_heads --ignore_locks --delete_unversioned_trees --disable-syntax-validation");
+    self.cmd_map.insert(String::from("gn"), "gen {}");
+    self
+      .cmd_map
+      .insert(String::from("incredibuild.cmd"), "{} {}");
+  }
+  fn args_of(&self, cmd: &str, args: &[&str]) -> Option<String> {
+    match self.cmd_map.get(cmd) {
+      // FIXME: format! only supports string literal.
+      Some(&arg_str) => format!(String::from(arg_str), args),
+      _ => None,
+    }
+  }
+}
+
+fn run() {
+  for cmd in build_cmd {
+    let output = process::Command::new(cmd.name)
+      .args(cmd.args)
+      .output()
+      .expect(&format!("{} failed", &cmd.name));
+    if !output.status.success() {
+      println!("{}", String::from_utf8_lossy(&output.stderr));
+      if !cmd.ignore {
+        break;
+      }
+    }
+  }
+}
+
+fn main() {
+  run();
+}
